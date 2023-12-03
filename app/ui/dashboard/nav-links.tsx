bba80b56f319a7c1c2c1e92a7168c11200f46662
HasegawaShuhei <sh1027.dev@gmail.com>
2023-12-03 23:39:25 +0900

    Linkの追加

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -3,6 +3,7 @@ import {
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -22,14 +23,18 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          // MEMO: Linkコンポーネントを使って、リンクを作成する。
+          // <Link>ブラウザのビューポートにコンポーネントが表示されるたびに、Next.js がリンクされたルートのコードをバックグラウンドで自動的にプリフェッチします。
+          // ユーザーがリンクをクリックするまでに、宛先ページのコードはすでにバックグラウンドで読み込まれており、これによりページがほぼ瞬時に遷移します。
+          // [https://nextjs.org/learn/dashboard-app/navigating-between-pages#automatic-code-splitting-and-prefetching]
+          <Link
             key={link.name}
             href={link.href}
             className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
