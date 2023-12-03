b4e88816c0fbddf8de54d664288ad596179fee2d
HasegawaShuhei <sh1027.dev@gmail.com>
2023-12-03 22:49:57 +0900

    セカンダリフォントの設定

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,10 @@
-import { Inter } from 'next/font/google';
+import { Inter, Lusitana } from 'next/font/google';
 
+// プライマリフォント
 export const inter = Inter({ subsets: ['latin'] });
+
+// セカンダリフォント
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
