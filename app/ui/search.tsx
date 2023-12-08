df0a6bd8ae685007edf75105bef53408bcc44311
HasegawaShuhei <sh1027.dev@gmail.com>
2023-12-08 23:20:47 +0900

    検索時にdebounceを行う

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,15 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term) => {
+    console.log(`Searching... ${term}`);
     // ?page=1&query=aのようなparams文字列を取得できます。
     const params = new URLSearchParams(searchParams);
     if (term) {
@@ -18,7 +20,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
     }
     // URLをユーザーの検索データで更新します
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
